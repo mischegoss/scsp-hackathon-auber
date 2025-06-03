fcd13600a96bcf2458cbe07ed619867b5e6a9fe1
mischegoss <mischegoss@gmail.com>
2025-06-03 16:41:43 -0400

    Add logo to NavBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,10 +61,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Narcan Connect',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           // Removed Tutorial and Blog navigation items
